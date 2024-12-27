ec72a90a63064cbb950840c94e74a7bf5c0752e1
Vaibhav-Kolhe14 <vaibhavkolhe1404@gmail.com>
2024-12-28 02:14:16 +0530

    context created

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { Link, useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 function Navbar() {
 
-    const [user, setUser] = useState(true);
-
+    const {user} = useContext(AppContext)
     const navigate = useNavigate()
 
   return (
@@ -17,16 +17,16 @@ function Navbar() {
     <div>
         {user ? 
         <div className='flex items-center gap-2 sm:gap-3'>
-            <button className='flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700'>
+            <button onClick={() => navigate('/buy')} className='flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700'>
                 <img src={assets.credit_star} alt='creditStar Icon' className='w-5'/>
                 <p className='text-xs sm:text-sm font-medium text-gray-600'>Credit left : 50</p>
             </button>
             <p className='text-gray-600 max-sm:hidden pl-4'>Hi, Vaibhav</p>
             <div className='relative group'>
                 <img src={assets.profile_icon} alt='user' className='w-10 drop-shadow'/>
-                <div className='absoulte hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
-                    <ul className='list-none'>
-                        <li>Logout</li>
+                <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
+                    <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
+                        <li className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
                     </ul>
                 </div>
             </div>
