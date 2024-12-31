1cf4888c12db502ece05d8f1ed6debb034a5af29
Vaibhav-Kolhe14 <vaibhavkolhe1404@gmail.com>
2024-12-31 23:22:16 +0530

    backend setuup

---
+++ server/src/controller/userController.js
@@ -0,0 +1,71 @@
+import ApiResponse from '../utils/ApiResponse.js'
+import ApiError from '../utils/ApiError.js'
+import asyncHandler from '../utils/asyncHandler.js'
+import User from '../model/userModel.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+
+const registerUser = asyncHandler( async(req, res) => {
+    try {
+        const {name, email, password} = req.body;
+
+        if(!name || !email || !password) {
+            return res.json({success: false, message: 'missing details'})
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const userData = {
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        const newUser = new User(userData)
+
+        const user = await newUser.save()
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+
+        res.status(200).json({success: true, token, user: {name: user.name}})
+    } catch (error) {
+        console.log("error in register user")
+        res.json({success: false, message: error.message})
+    }
+})
+
+const loginUser = asyncHandler( async(req, res) => {
+    try {
+        const {email, password} = req.body
+        const user = await User.findOne({email})
+
+        if(!user) {
+            return res.json({success: false, message: "user does not exist"})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(isMatch) {
+            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+            res.json({success: true, token, user: {name: user.name}})
+        } else {
+            return res({success: false, message: "Invalid credentials"})
+        }
+    } catch (error) {
+        console.log("Error in login user :: ", error)
+        res.json({success: false, message: error.message})
+    }
+})
+
+const userCredits = asyncHandler( async(req, res) => {
+    try {
+        // const
+    } catch (error) {
+        
+    }
+})
+
+
+export { registerUser, loginUser, userCredits };
---
+++ server/src/index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import { connectDB } from './db/connectDB.js'
+import userRouter from './routes/userRoutes.js'
+
+const PORT = process.env.PORT || 4000;
+
+const app = express()
+
+app.use(cors({
+    credentials: true,
+    origin: process.env.CORS_ORIGIN
+}))
+
+app.use(express.json())
+
+app.use('/api/user', userRouter)
+
+app.get('/', (req, res) => {
+    res.send("API WORKing")
+})
+
+
+connectDB()
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Server is running at port ${PORT}`)
+    })
+})
+.catch((error) => {
+    console.error("MongoDB Connection Failed:", error);
+})
